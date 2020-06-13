2c25edb70612f8aaf8f396ebb331297d842947c5
NachtaktiverHalbaffe <NachtaktiverHalbaffe@users.noreply.github.com>
2020-06-13 17:21:08 +0200

    PlatformIo fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
-#include <Arduino.h>
+#include  "GlobalDefinitions.h"
+#include  "SonicDriver.h"
+#include  "ServoDriver.h"
+#include  "RemoteControlDriver.h"
+#include  "MotorDriver.h"
+#include  "stdio.h"
+#include  "Arduino.h"
 
 void setup() {
   // put your setup code here, to run once:
