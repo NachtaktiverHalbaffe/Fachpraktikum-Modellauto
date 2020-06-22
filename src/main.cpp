910e8cb4d6f7f53c5e08e73a8644eb78fc9ec417
Rouven Rischert <rischertrouven@gmail.com>
2020-06-22 18:23:39 +0200

    get command for driving_direction and give it to the motor_driver so the car can drive with the right speed in the right direction

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,27 @@ void loop() {
 
   /*
    * ask for DRIVING_DIRECTION from RemoteDriver and give it to the MotorDriver
+   * GetRemoteSignal(DRIVING_DIRECTION) gives back a value between 0 and 1023
+   * if the value is higher than 523  -> forward
+   * if the value is lower than 500   -> reverse
+   * if value is between 523 and 500  -> stand
+   * 
+   * DriveForward only works with values between 510 and 255
+   * DriveReverse with values between 255 and 0
+   * 
+   * The factor 2 is to get the values from GetRemoteSignal into the right values for DriveForward/DriveReverse
+   * 
    */
+  static int driving_main;
+  driving_main = GetRemoteSignal(DRIVING_DIRECTION)/2;
+  if (GetDrivingDirection()==0){
+    DriveForward(driving_main);
+  }
+  else if (GetDrivingDirection()==1){
+    DriveForward(driving_main);
+  }
+  else if (GetDrivingDirection()==2){
+    MotorIdle();
+  }
 
-
-
-
-}
+  }
